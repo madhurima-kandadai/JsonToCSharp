579bedc2232409db7ccf87981fa9a569a4811543
Kandadai Madhurima Yasaswini <madhurima.kandadai@gmail.com>
2016-07-15 15:13:14 +0530

    Add project files.

---
+++ JsonToCSharpCode/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JsonToCSharpCode.MvcApplication" Language="C#" %>
---
+++ JsonToCSharpCode/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JsonToCSharpCode.Startup))]
+namespace JsonToCSharpCode
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
