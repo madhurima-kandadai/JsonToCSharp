c4d02381d8d234a2d9de878af195bedb50af8238
Kandadai Madhurima Yasaswini <madhurima.kandadai@gmail.com>
2016-07-20 17:56:37 +0530

    Added front end data

---
+++ JsonToCSharpCode/Scripts/AppScripts/jsonController.js
@@ -0,0 +1,23 @@
+﻿app.controller('jsonController', function ($http, $scope) {
+
+    $scope.models = [];
+
+    $scope.dataTypes = [{
+        value: 'int',
+        name: 'Integer'
+    },
+    {
+        value: 'string',
+        name: 'String'
+    },
+    {
+        value: 'DateTime',
+        name: 'Date Time'
+    }
+    ];
+
+    $scope.SaveModelDetails = function () {
+
+    };
+
+});
