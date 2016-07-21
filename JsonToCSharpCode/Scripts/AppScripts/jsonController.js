fb592ef491484f70df0862869708a7712d6d8527
Kandadai Madhurima Yasaswini <madhurima.kandadai@gmail.com>
2016-07-21 15:16:57 +0530

    Added add functionality and show json functionality partially

--- JsonToCSharpCode/Scripts/AppScripts/jsonController.js
+++ JsonToCSharpCode/Scripts/AppScripts/jsonController.js
@@ -1,17 +1,7 @@
-﻿app.controller('jsonController', function ($http, $scope) {
+﻿app.controller('jsonController', function ($scope, $http) {
 
-    $scope.models = [
-        {
-            "modelName": "",
-            "required": [{}],
-            "properties":
-                    [{
-                        "propertyName": "",
-                        "type": "",
-                        "required": ""
-                    }]
-        }
-    ];
+    $scope.swaggerJson = [];
+    $scope.models = [];
 
     $scope.dataTypes = [{
         value: 'int',
@@ -27,12 +17,78 @@
     }
     ];
 
-    $scope.SaveModelDetails = function () {
+    $scope.ViewJson = function () {
+        $scope.swaggerJson = [];
+        if ($scope.models.length !== 0) {
+            var value = $scope.GenerateJson();
+            $scope.jsonObject = JSON.stringify($scope.swaggerJson, null, 2);
+            console.log($scope.jsonObject);
+        }
+    };
+
+    $scope.GenerateJson = function () {
+        angular.forEach($scope.models, function (model) {
+            var modelName = model.model;
+            $scope.swaggerJson.push({
+                "model": model.model,
+                "required": [],
+                "properties": []
+            });
+            var index = $scope.swaggerJson.findIndex(x => x.model == model.model);
+            angular.forEach(model.properties, function (prop) {
+                var propName = prop.name;
+                if (prop.required === true) {
+                    $scope.swaggerJson[index].required.push(propName);
+                }
+                $scope.swaggerJson[index].properties.push({
+                    "name": prop.name,
+                    "type": prop.dataType,
+                    "required": prop.required
+                });
+            });
+        });
+        return $scope.swaggerJson;
+    };
+
+    $scope.EditProperty = function () {
 
     };
+
+    $scope.DeleteProperty = function () {
+
+    }
 
     $scope.AddPropertyToModel = function () {
+        if (JSON.stringify($scope.models).indexOf(JSON.stringify($scope.modelName)) == -1) {
+            $scope.dataTypes.push({ value: $scope.modelName, name: $scope.modelName })
+            $scope.models.push({
+                "model": $scope.modelName,
+                "properties": [{
+                    "name": $scope.propertyName,
+                    "dataType": $scope.dataType,
+                    "required": $scope.required == null ? false : $scope.required
+                }]
+            });
+        }
+        else {
+            var index = $scope.models.findIndex(x => x.model.toLowerCase() == $scope.modelName.toLowerCase());
+            if (index != -1) {
+                var propIndex = $scope.models[index].properties.findIndex(x => x.name.toLowerCase() == $scope.propertyName.toLowerCase());
+                if (propIndex == -1) {
+                    $scope.models[index].properties.push({
+                        "name": $scope.propertyName,
+                        "dataType": $scope.dataType,
+                        "required": $scope.required == null ? false : $scope.required
+                    });
+                }
+                else {
+                    alert("Duplicate property !~!!");
+                }
+            }
 
+        }
+        $scope.propertyName = '';
+        $scope.dataType = "";
+        $scope.required = null;
     };
-
 });
