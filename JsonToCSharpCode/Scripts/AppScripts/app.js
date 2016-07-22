77fe1214a8b7a7a1423c3d91ba9ad8a5a20796f9
Kandadai Madhurima Yasaswini <madhurima.kandadai@gmail.com>
2016-07-22 14:50:01 +0530

    Added grid related files.

--- JsonToCSharpCode/Scripts/AppScripts/app.js
+++ JsonToCSharpCode/Scripts/AppScripts/app.js
@@ -1,1 +1,12 @@
-﻿var app = angular.module('codeDev', []);
+﻿var app = angular.module('codeDev', [
+    'ui.grid',
+    'ui.grid.pagination',
+    'ui.bootstrap',
+    'ui.grid.selection',
+    'ui.grid.edit',
+    'ui.grid.rowEdit',
+    'ui.grid.cellNav',
+    'ui.grid.grouping',
+    'ui.grid.expandable',
+    'ui.grid.pinning',
+    'ngDialog']);
